4df8609f54e676237a869697f6fd421d3652231d
Koushika <koushiks6212117@gmail.com>
2026-07-26 22:34:59 +0530

    login completed

--- login.py
+++ login.py
@@ -1,6 +1,36 @@
-print("Welcome to FoodExpress")
+def login():
 
-username = input("Enter Username: ")
-password = input("Enter Password: ")
+    print("\n===== FoodExpress Login =====")
 
-print("Login Successful")
+    username = input("Enter Username : ")
+    password = input("Enter Password : ")
+
+    print("\nSelect Role")
+    print("1. Customer")
+    print("2. Restaurant Owner")
+    print("3. Delivery Agent")
+    print("4. Admin")
+
+    choice = input("Enter your choice : ")
+
+    if choice == "1":
+        role = "Customer"
+
+    elif choice == "2":
+        role = "Restaurant Owner"
+
+    elif choice == "3":
+        role = "Delivery Agent"
+
+    elif choice == "4":
+        role = "Admin"
+
+    else:
+        print("Invalid Role")
+        return None
+
+    print("\nLogin Successful")
+    print("Welcome", username)
+    print("Role :", role)
+
+    return role
